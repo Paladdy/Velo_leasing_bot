28a9620bb82f6f89cf48c24dc6818dd7820d4117
pknowledge <xyz@gmail.com>
2025-10-29 15:41:56 +0400

    fix: Добавлено состояние choosing_language в RegistrationStates
    - Добавлено отсутствующее состояние choosing_language
    - Исправляет ошибку AttributeError при начале регистрации

--- bot/states/registration.py
+++ bot/states/registration.py
@@ -2,6 +2,7 @@ from aiogram.fsm.state import State, StatesGroup
 
 
 class RegistrationStates(StatesGroup):
+    choosing_language = State()
     waiting_for_name = State()
     waiting_for_phone = State()
     choosing_document_type = State()
